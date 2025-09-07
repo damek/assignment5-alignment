2bcfac15dcbf27fbd72871a4f25224f327d7f861
damek <damek@users.noreply.github.com>
2025-09-07 17:27:25 -0400

    printing the first entry of dataset.

--- cs336_alignment/math_baseline.py
+++ cs336_alignment/math_baseline.py
@@ -1,4 +1,13 @@
-from datasets import load_dataset
+# first load the data set from ../data/gsm8k/gsm8k-test.jsonl
+import json
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-ds = load_dataset("openai/gsm8k", "main")
+with open("../data/gsm8k/gsm8k-test.jsonl", "r") as f:
+    for line in f:
+        data = json.loads(line)
+        print(data)
+        break
 
+for data in ds:
+    print(data)
+    break
