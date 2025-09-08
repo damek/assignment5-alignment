c885b07a7c44499c7124357a17308a9655185aef
damek <damek@users.noreply.github.com>
2025-09-08 16:16:07 -0400

    s

--- cs336_alignment/tokenize_prompt_and_output.py
+++ cs336_alignment/tokenize_prompt_and_output.py
@@ -19,7 +19,7 @@ def tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer):
     input_ids = torch.cat([prompt_tokenize.input_ids, output_tokenize.input_ids], dim=1)
     labels = input_ids[:, 1:]# everything but first token.
     input_ids = input_ids[:, :-1] # everything but last token.
-    response_mask = torch.zeros(input_ids.shape[1]-1, dtype=bool) # mask only the response tokens in the labels.
+    response_mask = torch.zeros(input_ids.shape[1], dtype=bool) # mask only the response tokens in the labels.
     response_mask[len(prompt_strs)-1:] = True # the first token was cut off because response_mask is aligned with labels.
     # response_mask[:, :len(prompt_strs)-1] = False # the first token was cut off because response_mask is aligned with labels.
     return {"input_ids": input_ids, "labels": labels, "response_mask": response_mask}
