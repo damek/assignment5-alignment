4b8f72f2d90ca7d8f1ae43fa2026f51672e1e16d
damek <damek@users.noreply.github.com>
2025-09-08 16:52:54 -0400

    try it out

--- cs336_alignment/tokenize_prompt_and_output.py
+++ cs336_alignment/tokenize_prompt_and_output.py
@@ -14,8 +14,8 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
 def tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer): 
-    prompt_tokenize = tokenizer(prompt_strs, padding=False)
-    output_tokenize = tokenizer(output_strs, padding=False)
+    prompt_tokenize = tokenizer(prompt_strs, padding=False, add_special_tokens=False)
+    output_tokenize = tokenizer(output_strs, padding=False, add_special_tokens=False)
     ids = [p + o for p, o in zip(prompt_tokenize["input_ids"], output_tokenize["input_ids"])]
     # now add padding using the tokenizer
     sequences_to_pad = [{"input_ids": x} for x in ids]
