b70794ed63709778358c4c11ba650b1dab27a076
damek <damek@users.noreply.github.com>
2025-09-08 16:21:58 -0400

    s

--- cs336_alignment/tokenize_prompt_and_output.py
+++ cs336_alignment/tokenize_prompt_and_output.py
@@ -14,12 +14,12 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
 def tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer): 
-    prompt_tokenize = tokenizer(prompt_strs, return_tensors="pt", padding=True)
-    output_tokenize = tokenizer(output_strs, return_tensors="pt", padding=True)
+    prompt_tokenize = tokenizer(prompt_strs, return_tensors="pt")
+    output_tokenize = tokenizer(output_strs, return_tensors="pt")
     input_ids = torch.cat([prompt_tokenize.input_ids, output_tokenize.input_ids], dim=1)
     labels = input_ids[:, 1:]# everything but first token.
     input_ids = input_ids[:, :-1] # everything but last token.
     response_mask = torch.zeros(input_ids.shape[1], dtype=bool) # mask only the response tokens in the labels.
-    response_mask[len(prompt_strs)-1:] = True # the first token was cut off because response_mask is aligned with labels.
-    # response_mask[:, :len(prompt_strs)-1] = False # the first token was cut off because response_mask is aligned with labels.
+    response_mask[prompt_strs.shape[1]-1:] = True # We're dealing with the labels and since we left off the first token, the response starts one step later
+
     return {"input_ids": input_ids, "labels": labels, "response_mask": response_mask}
