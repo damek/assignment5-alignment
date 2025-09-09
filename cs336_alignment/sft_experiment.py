62367da32b3470aab20d5c6caf8b08e2f3c0dd48
damek <damek@users.noreply.github.com>
2025-09-09 17:41:00 -0400

    s

--- cs336_alignment/sft_experiment.py
+++ cs336_alignment/sft_experiment.py
@@ -104,7 +104,9 @@ for epoch in range(NUM_EPOCHS):
         # Compute the policy log probs
         policy_log_probs = utils.get_response_log_probs(model, input_ids, labels, return_token_entropy=False)["log_probs"]
         vllm_utils.load_policy_into_vllm_instance(model, vllm_model)
-        rewards, _ = utils.evaluate_vllm(vllm_model, r1_zero_reward_fn, train_dataset_r1_zero[batch_indices.tolist()], eval_sampling_params)
+        idx = batch_indices.tolist() if isinstance(batch_indices, torch.Tensor) else list(batch_indices)
+        batch = [train_dataset_r1_zero[i] for i in idx] # batch is a list of dictionaries
+        rewards, _ = utils.evaluate_vllm(vllm_model, r1_zero_reward_fn, batch, eval_sampling_params)
         if i == 0:
             ema_reward = rewards["reward"]
             ema_format_reward = rewards["format_reward"]
