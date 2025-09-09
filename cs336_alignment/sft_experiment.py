5ee248b7782b735242a23e7cab56c6280c01d98d
damek <damek@users.noreply.github.com>
2025-09-09 19:37:46 -0400

    s

--- cs336_alignment/sft_experiment.py
+++ cs336_alignment/sft_experiment.py
@@ -28,6 +28,7 @@ def get_args():
     parser.add_argument("--gradient_accumulation_steps", type=int, default=2)
     parser.add_argument("--lr", type=float, default=1e-4)
     parser.add_argument("--num_epochs", type=int, default=1)
+    parser.add_argument("--print_reward_every", type=int, default=10)
     return parser.parse_args()
 ## later put these into argparse.
 
@@ -40,6 +41,7 @@ LR = args.lr
 # WARMUP_STEPS = args.warmup_steps
 NUM_EPOCHS = args.num_epochs
 WANDB_PROJECT = "sft-experiment"
+PRINT_REWARD_EVERY = args.print_reward_every
 
 wandb.init(project="sft-experiment") 
 # Setup wandb metrics
@@ -94,7 +96,7 @@ for epoch in range(NUM_EPOCHS):
     ema_format_reward = float("inf")
     for i in range(len(train_dataset) // BATCH_SIZE):
         print(f"Epoch {epoch}, Batch {i}/{len(train_dataset) // BATCH_SIZE}")
-        print(f"EMA Loss: {ema_loss:.4f}, EMA Reward: {ema_reward:.4f}, EMA Format Reward: {ema_format_reward:.4f}")
+        print(f"EMA Loss: {ema_loss:.4f}")
         # Compute a batch of training examples
         batch_indices = shuffle_indices[i * BATCH_SIZE:(i + 1) * BATCH_SIZE]
         input_ids = train_input_ids[batch_indices]
@@ -103,16 +105,22 @@ for epoch in range(NUM_EPOCHS):
 
         # Compute the policy log probs
         policy_log_probs = utils.get_response_log_probs(model, input_ids, labels, return_token_entropy=False)["log_probs"]
-        vllm_utils.load_policy_into_vllm_instance(model, vllm_model)
-        idx = batch_indices.tolist() if isinstance(batch_indices, torch.Tensor) else list(batch_indices)
-        batch = [train_dataset_r1_zero[i] for i in idx] # batch is a list of dictionaries
-        rewards, _ = utils.evaluate_vllm(vllm_model, r1_zero_reward_fn, batch, eval_sampling_params)
-        if i == 0:
-            ema_reward = torch.tensor([x["reward"] for x in rewards]).sum() 
-            ema_format_reward = torch.tensor([x["format_reward"] for x in rewards]).sum()
-        else:
-            ema_reward = 0.9 * ema_reward + 0.1 * torch.tensor([x["reward"] for x in rewards]).sum()
-            ema_format_reward = 0.9 * ema_format_reward + 0.1 * torch.tensor([x["format_reward"] for x in rewards]).sum()   
+
+        
+        # every PRINT_REWARD_EVERY steps, sample generations and print the reward from the last PRINT_REWARD_EVERY steps, then reset the rewards
+        if (i+1) % PRINT_REWARD_EVERY == 0:
+            print_batch = shuffle_indices[(i-PRINT_REWARD_EVERY) * BATCH_SIZE:(i) * BATCH_SIZE]
+            vllm_utils.load_policy_into_vllm_instance(model, vllm_model)
+            idx = print_batch.tolist() if isinstance(print_batch, torch.Tensor) else list(print_batch)
+            batch = [train_dataset_r1_zero[i] for i in idx] # batch is a list of dictionaries
+            rewards, _ = utils.evaluate_vllm(vllm_model, r1_zero_reward_fn, batch, eval_sampling_params)
+            if i == 0:
+                ema_reward = torch.tensor([x["reward"] for x in rewards]).sum() 
+                ema_format_reward = torch.tensor([x["format_reward"] for x in rewards]).sum()
+            else:
+                ema_reward = 0.9 * ema_reward + 0.1 * torch.tensor([x["reward"] for x in rewards]).sum()
+                ema_format_reward = 0.9 * ema_format_reward + 0.1 * torch.tensor([x["format_reward"] for x in rewards]).sum()   
+            print(f"EMA Reward: {ema_reward:.4f}, EMA Format Reward: {ema_format_reward:.4f}")
 
         # Compute the loss
         loss, _ = utils.sft_microbatch_train_step(policy_log_probs, response_mask, GRADIENT_ACCUMULATION_STEPS)
