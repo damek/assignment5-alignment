ef5445fd76802ff6744d362fcb33ecdd0ca2fa62
damek <damek@users.noreply.github.com>
2025-09-09 17:07:03 -0400

    s

--- cs336_alignment/sft_experiment.py
+++ cs336_alignment/sft_experiment.py
@@ -69,7 +69,7 @@ train_response_mask = train_dataset_tokenized["response_mask"].to(device_hf)
 with torch.no_grad():
     vllm_utils.load_policy_into_vllm_instance(model, vllm_model)
     print("model device after load into vllm ", model.device)
-    log_generations_dict = utils.log_generations(vllm_model, model, tokenizer, eval_dataset_r1_zero)
+    log_generations_dict = utils.log_generations(vllm_model, model, tokenizer, eval_dataset_r1_zero, batch_size=BATCH_SIZE)
     wandb.log(log_generations_dict)
     histogram = utils.count_histogram(log_generations_dict["examples"])
     print("histogram: ", histogram)
