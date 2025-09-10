88d24dce6f813dd8d0d80c103ce2838cc43a98be
damek <damek@users.noreply.github.com>
2025-09-10 15:48:12 -0400

    OK added a batch

--- cs336_alignment/expert_iteration.py
+++ cs336_alignment/expert_iteration.py
@@ -100,58 +100,59 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=LR)
 eval_sampling_params = SamplingParams(temperature=1.0, top_p=1.0, max_tokens=1024)
 # check the eval:    
 # So annoying thing here is that we need to retokenizer our dataset every time we do an expert iteration.
-for expert_iteration in range(NUM_EXPERT_ITERATIONS):
-    shuffle_indices = torch.randperm(len(train_dataset))
-    # now we're going to choose a subset of them to make our expert iteration batch
-    expert_batch_indices = shuffle_indices[:EXPERT_BATCH_SIZE]
-    expert_batch_r1_zero = [train_dataset_r1_zero[i] for i in expert_batch_indices]
-    expert_batch = utils.make_expert_iteration_batch(vllm_model, expert_batch_r1_zero, EXPERT_BATCH_SIZE, NUM_ROLLOUTS)
-    expert_batch_tokenized = utils.tokenize_prompt_and_output([data["prompt"] for data in expert_batch], [data["response"] for data in expert_batch], tokenizer)
-    input_ids = expert_batch_tokenized["input_ids"].to(device_hf)
-    labels = expert_batch_tokenized["labels"].to(device_hf)
-    response_mask = expert_batch_tokenized["response_mask"].to(device_hf)
-    
-    # now we're going to construct to do SFT on this dataset.
+for BATCH_SIZE in [512, 1024, 2048]:
+    for expert_iteration in range(NUM_EXPERT_ITERATIONS):
+        shuffle_indices = torch.randperm(len(train_dataset))
+        # now we're going to choose a subset of them to make our expert iteration batch
+        expert_batch_indices = shuffle_indices[:EXPERT_BATCH_SIZE]
+        expert_batch_r1_zero = [train_dataset_r1_zero[i] for i in expert_batch_indices]
+        expert_batch = utils.make_expert_iteration_batch(vllm_model, expert_batch_r1_zero, EXPERT_BATCH_SIZE, NUM_ROLLOUTS)
+        expert_batch_tokenized = utils.tokenize_prompt_and_output([data["prompt"] for data in expert_batch], [data["response"] for data in expert_batch], tokenizer)
+        input_ids = expert_batch_tokenized["input_ids"].to(device_hf)
+        labels = expert_batch_tokenized["labels"].to(device_hf)
+        response_mask = expert_batch_tokenized["response_mask"].to(device_hf)
+        
+        # now we're going to construct to do SFT on this dataset.
 
-    for epoch in range(NUM_EPOCHS):
-        shuffle_expert_indices = torch.randperm(len(expert_batch))
-        ema_loss = float("inf")
-        ema_reward = float("inf")
-        ema_format_reward = float("inf")
-        for i in range(len(expert_batch) // BATCH_SIZE):
-            print(f"Expert Iteration {expert_iteration}, Epoch {epoch}, Batch {i}/{len(expert_batch) // BATCH_SIZE}")
-            print(f"EMA Loss: {ema_loss:.4f}")
-            last_index = min(i * BATCH_SIZE + BATCH_SIZE, len(expert_batch))
-            batch_indices = shuffle_expert_indices[i * BATCH_SIZE:last_index]
-            input_ids_batch = input_ids[batch_indices].to(device_hf)
-            labels_batch = labels[batch_indices].to(device_hf)
-            response_mask_batch = response_mask[batch_indices].to(device_hf)
+        for epoch in range(NUM_EPOCHS):
+            shuffle_expert_indices = torch.randperm(len(expert_batch))
+            ema_loss = float("inf")
+            ema_reward = float("inf")
+            ema_format_reward = float("inf")
+            for i in range(len(expert_batch) // BATCH_SIZE):
+                print(f"Batch Size {BATCH_SIZE}, Expert Iteration {expert_iteration}, Epoch {epoch}, Batch {i}/{len(expert_batch) // BATCH_SIZE}")
+                print(f"EMA Loss: {ema_loss:.4f}")
+                last_index = min(i * BATCH_SIZE + BATCH_SIZE, len(expert_batch))
+                batch_indices = shuffle_expert_indices[i * BATCH_SIZE:last_index]
+                input_ids_batch = input_ids[batch_indices].to(device_hf)
+                labels_batch = labels[batch_indices].to(device_hf)
+                response_mask_batch = response_mask[batch_indices].to(device_hf)
 
-            # Compute the policy log probs
-            policy_log_probs = utils.get_response_log_probs(model, input_ids_batch, labels_batch, return_token_entropy=False)["log_probs"]
+                # Compute the policy log probs
+                policy_log_probs = utils.get_response_log_probs(model, input_ids_batch, labels_batch, return_token_entropy=False)["log_probs"]
 
-            # Compute the loss
-            loss, _ = utils.sft_microbatch_train_step(policy_log_probs, response_mask_batch, GRADIENT_ACCUMULATION_STEPS)
-            if i == 0:
-                ema_loss = loss.item()
-            else:
-                ema_loss = 0.9 * ema_loss + 0.1 * loss.item()
-            wandb.log({"ema_loss": ema_loss})
-            if (i+1) % GRADIENT_ACCUMULATION_STEPS == 0:
-                optimizer.step()
-                optimizer.zero_grad()
-    
-        with torch.no_grad():
-            print(f"Expert Iteration {expert_iteration}, Epoch {epoch}, Evaluating...")
-            vllm_utils.load_policy_into_vllm_instance(model, vllm_model)
-            log_generations_dict = utils.log_generations(vllm_model, model, tokenizer, eval_dataset_r1_zero, batch_size=BATCH_SIZE)
-            wandb.log(log_generations_dict) # index x by epoch
-            histogram = utils.count_histogram(log_generations_dict["examples"])
-            print("histogram: ", histogram)
-            val_accuracy = histogram["correct with both format and answer reward 1"] / sum(histogram.values())
-            print("Percentage of correct examples: ", val_accuracy)
-            wandb.log({"val_accuracy": val_accuracy, "epoch": epoch, "expert_iteration": expert_iteration}) 
+                # Compute the loss
+                loss, _ = utils.sft_microbatch_train_step(policy_log_probs, response_mask_batch, GRADIENT_ACCUMULATION_STEPS)
+                if i == 0:
+                    ema_loss = loss.item()
+                else:
+                    ema_loss = 0.9 * ema_loss + 0.1 * loss.item()
+                wandb.log({"ema_loss": ema_loss})
+                if (i+1) % GRADIENT_ACCUMULATION_STEPS == 0:
+                    optimizer.step()
+                    optimizer.zero_grad()
+        
+            with torch.no_grad():
+                print(f"Expert Iteration {expert_iteration}, Epoch {epoch}, Batch Size {BATCH_SIZE}, Evaluating...")
+                vllm_utils.load_policy_into_vllm_instance(model, vllm_model)
+                log_generations_dict = utils.log_generations(vllm_model, model, tokenizer, eval_dataset_r1_zero, batch_size=BATCH_SIZE)
+                wandb.log(log_generations_dict) # index x by epoch
+                histogram = utils.count_histogram(log_generations_dict["examples"])
+                print("histogram: ", histogram)
+                val_accuracy = histogram["correct with both format and answer reward 1"] / sum(histogram.values())
+                print("Percentage of correct examples: ", val_accuracy)
+                wandb.log({"val_accuracy": val_accuracy, "epoch": epoch, "expert_iteration": expert_iteration, "batch_size": BATCH_SIZE}) 
 
 
-            utils.print_format_reward_1_answer_reward_1(log_generations_dict["examples"], 3)
+                utils.print_format_reward_1_answer_reward_1(log_generations_dict["examples"], 3)
 
