72c38f83348a4fcaaa39a666f4cf01dbb32d5a4f
damek <damek@users.noreply.github.com>
2025-09-11 10:03:23 -0400

    s

--- cs336_alignment/expert_iteration.sh
+++ cs336_alignment/expert_iteration.sh
@@ -6,12 +6,12 @@
 
 BATCH_SIZE=10
 GRADIENT_ACCUMULATION_STEPS=2
-MAX_SEQ_LEN=512
+MAX_TOKENS_TRAIN=512
 for NUM_EPOCHS in 2; do
     for NUM_ROLLOUTS in 4 6; do
         for NUM_EXPERT_ITERATIONS in 5; do
             for EXPERT_BATCH_SIZE in 512 1024 2048; do
-                uv run expert_iteration.py --num_rollouts $NUM_ROLLOUTS --num_expert_iterations $NUM_EXPERT_ITERATIONS --expert_batch_size $EXPERT_BATCH_SIZE --num_epochs $NUM_EPOCHS --batch_size $BATCH_SIZE --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS --max_seq_len $MAX_SEQ_LEN
+                uv run expert_iteration.py --num_rollouts $NUM_ROLLOUTS --num_expert_iterations $NUM_EXPERT_ITERATIONS --expert_batch_size $EXPERT_BATCH_SIZE --num_epochs $NUM_EPOCHS --batch_size $BATCH_SIZE --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS --max_tokens_train $MAX_TOKENS_TRAIN
             done
         done
     done
