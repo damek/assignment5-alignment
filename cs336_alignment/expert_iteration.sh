890e86c2a133e11060b7151974610f8bd4744af1
damek <damek@users.noreply.github.com>
2025-09-10 21:40:18 -0400

    s

--- cs336_alignment/expert_iteration.sh
+++ cs336_alignment/expert_iteration.sh
@@ -5,7 +5,7 @@
 # batch size 10
 
 for NUM_EPOCHS in 2; do
-    for NUM_ROLLOUTS in 2 4; do
+    for NUM_ROLLOUTS in 4 6; do
         for NUM_EXPERT_ITERATIONS in 5; do
             for EXPERT_BATCH_SIZE in 512 1024 2048; do
                 uv run expert_iteration.py --num_rollouts $NUM_ROLLOUTS --num_expert_iterations $NUM_EXPERT_ITERATIONS --expert_batch_size $EXPERT_BATCH_SIZE --num_epochs $NUM_EPOCHS
