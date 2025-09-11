a7f622a0cebbbea4b02417efdb77948bead83cea
damek <damek@users.noreply.github.com>
2025-09-11 09:58:37 -0400

    now I think we can run it

--- cs336_alignment/expert_iteration.sh
+++ cs336_alignment/expert_iteration.sh
@@ -4,11 +4,13 @@
 # number of epochs 1, 2
 # batch size 10
 
+BATCH_SIZE=5
+GRADIENT_ACCUMULATION_STEPS=4
 for NUM_EPOCHS in 2; do
     for NUM_ROLLOUTS in 4 6; do
         for NUM_EXPERT_ITERATIONS in 5; do
             for EXPERT_BATCH_SIZE in 512 1024 2048; do
-                uv run expert_iteration.py --num_rollouts $NUM_ROLLOUTS --num_expert_iterations $NUM_EXPERT_ITERATIONS --expert_batch_size $EXPERT_BATCH_SIZE --num_epochs $NUM_EPOCHS
+                uv run expert_iteration.py --num_rollouts $NUM_ROLLOUTS --num_expert_iterations $NUM_EXPERT_ITERATIONS --expert_batch_size $EXPERT_BATCH_SIZE --num_epochs $NUM_EPOCHS --batch_size $BATCH_SIZE --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS
             done
         done
     done
