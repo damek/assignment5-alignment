a8366a41ed912c56f086f22a025d046ab7e0c7a4
damek <damek@users.noreply.github.com>
2025-09-11 17:01:11 -0400

    s

--- cs336_alignment/sft_experiment.sh
+++ cs336_alignment/sft_experiment.sh
@@ -2,6 +2,8 @@
 
 # run for num_sft_examples 128, 256, 512, and None (which is all)
 
-for NUM_SFT_EXAMPLES in 128 256 512 None; do
+for NUM_SFT_EXAMPLES in 128 256 512; do
     uv run sft_experiment.py --num_sft_examples $NUM_SFT_EXAMPLES
 done
+
+uv run sft_experiment.py
