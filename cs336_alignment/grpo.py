737901bab7404c1ea5aec2f8393606ca10eaed9f
damek <damek@users.noreply.github.com>
2025-09-12 16:59:23 -0400

    Testing the training step

--- cs336_alignment/grpo.py
+++ cs336_alignment/grpo.py
@@ -106,3 +106,27 @@ def masked_mean(
         return torch.sum(tensor * mask, dim=dim) / torch.sum(mask, dim=dim)
     else:
         return torch.sum(tensor * mask) / torch.sum(mask)
+
+def grpo_microbatch_train_step(
+    policy_log_probs: torch.Tensor,
+    response_mask: torch.Tensor,
+    gradient_accumulation_steps: int,
+    loss_type: Literal["no_baseline", "reinforce_with_baseline", "grpo_clip"],
+    raw_rewards: torch.Tensor | None = None,
+    advantages: torch.Tensor | None = None,
+    old_log_probs: torch.Tensor | None = None,
+    cliprange: float | None = None,
+    ) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
+
+    loss = compute_policy_gradient_loss(
+        policy_log_probs=policy_log_probs,
+        loss_type=loss_type,
+        raw_rewards=raw_rewards,
+        advantages=advantages,
+        old_log_probs=old_log_probs,
+        cliprange=cliprange,
+    )
+    loss = masked_mean(loss, response_mask)
+    loss /= max(1, gradient_accumulation_steps)
+    loss.backward()
+    return loss, {"loss": loss}
