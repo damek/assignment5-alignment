9f35e261f8cc5aea802a4e0a3d47216a829cfaf6
damek <damek@users.noreply.github.com>
2025-09-12 16:49:33 -0400

    s

--- tests/adapters.py
+++ tests/adapters.py
@@ -9,7 +9,7 @@ from torch.utils.data import Dataset
 from transformers import PreTrainedTokenizerBase
 from cs336_alignment.utils import tokenize_prompt_and_output
 from cs336_alignment.utils import compute_entropy, get_response_log_probs, masked_normalize, sft_microbatch_train_step
-from cs336_alignment.grpo import compute_group_normalized_rewards, compute_naive_policy_gradient_loss, compute_grpo_clip_loss
+from cs336_alignment.grpo import compute_group_normalized_rewards, compute_naive_policy_gradient_loss, compute_grpo_clip_loss, compute_policy_gradient_loss
 
 def run_tokenize_prompt_and_output(
     prompt_strs: list[str],
@@ -192,8 +192,14 @@ def run_compute_policy_gradient_loss(
     """
     Wrapper that delegates to the appropriate policy gradient loss function above.
     """
-    raise NotImplementedError
-
+    return compute_policy_gradient_loss(
+        policy_log_probs=policy_log_probs,
+        loss_type=loss_type,
+        raw_rewards=raw_rewards,
+        advantages=advantages,
+        old_log_probs=old_log_probs,
+        cliprange=cliprange,
+    )
 
 def run_masked_mean(tensor: torch.Tensor, mask: torch.Tensor, dim: int | None = None) -> torch.Tensor:
     """Compute the mean of the tensor along a dimension,
