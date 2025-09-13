a12fa88268c995bd13c427a1fdd021dc9b10951c
damek <damek@users.noreply.github.com>
2025-09-12 21:27:07 -0400

    s

--- cs336_alignment/grpo_train_loop.py
+++ cs336_alignment/grpo_train_loop.py
@@ -142,31 +142,29 @@ for grpo_iteration in range(NUM_GRPO_ITERATIONS):
     utils.mem("after HF policy load")
     # Do rollouts from VLLM model
     rewards, prompt_response_answer_flattened  = grpo.sample_rollouts(vllm_model, train_dataset_r1_zero_grpo_step, GROUP_SIZE, r1_zero_reward_fn, MAX_TOKENS_TRAIN, TEMPERATURE, TOP_P)
-    print("reward.shape: ", len(rewards))
-    print("prompt_response_answer_flattened.shape: ", len(prompt_response_answer_flattened))
+
     # Rollouts
     tokenize_samples = utils.tokenize_prompt_and_output([data["prompt"] for data in prompt_response_answer_flattened], [data["response"] for data in prompt_response_answer_flattened], tokenizer)
     input_ids = tokenize_samples["input_ids"].to(device_hf)
     labels = tokenize_samples["labels"].to(device_hf)
     response_mask = tokenize_samples["response_mask"].to(device_hf)
-    print("input_ids.shape: ", input_ids.shape)
-    print("labels.shape: ", labels.shape)
-    print("response_mask.shape: ", response_mask.shape)
+
     advantages, raw_rewards, metadata = grpo.compute_group_normalized_rewards(r1_zero_reward_fn, prompt_response_answer_flattened, [data["answer"] for data in prompt_response_answer_flattened], GROUP_SIZE, ADVANTAGE_EPS, USE_STD_NORMALIZATION)
     print("advantages.shape: ", advantages.shape)
     print("raw_rewards.shape: ", raw_rewards.shape)
     # move to device
     advantages = advantages.to(device_hf)
     raw_rewards = raw_rewards.to(device_hf)
-    old_log_probs = torch.zeros(input_ids.shape, dtype=torch.float32).to(device_hf)
-    with torch.no_grad():
-        for i in range(0, TRAIN_BATCH_SIZE // micro_train_batch_size):
-            last_index = min((i+1) * micro_train_batch_size, TRAIN_BATCH_SIZE)
-            batch_indices = torch.arange(i * micro_train_batch_size, last_index)
-            input_ids_batch = input_ids[batch_indices, :]
-            labels_batch = labels[batch_indices, :]
-            response_mask_batch = response_mask[batch_indices, :]
-            old_log_probs[batch_indices, :] = utils.get_response_log_probs(model, input_ids_batch, labels_batch, return_token_entropy=False)["log_probs"]
+    old_log_probs = torch.empty(input_ids.shape, dtype=torch.float32).to(device_hf)
+    if LOSS_TYPE == "grpo_clip":
+        with torch.no_grad():
+            for i in range(0, TRAIN_BATCH_SIZE // micro_train_batch_size):
+                last_index = min((i+1) * micro_train_batch_size, TRAIN_BATCH_SIZE)
+                batch_indices = torch.arange(i * micro_train_batch_size, last_index)
+                input_ids_batch = input_ids[batch_indices, :]
+                labels_batch = labels[batch_indices, :]
+                response_mask_batch = response_mask[batch_indices, :]
+                old_log_probs[batch_indices, :] = utils.get_response_log_probs(model, input_ids_batch, labels_batch, return_token_entropy=False)["log_probs"]
     
     histogram = utils.count_histogram(rewards)
     print("histogram: ", histogram)
