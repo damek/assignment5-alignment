bc44c6c9cdc0bd43135b17832cf7a317f1c425f8
damek <damek@users.noreply.github.com>
2025-09-13 19:23:53 -0400

    s

--- cs336_alignment/grpo_learning_rate.sh
+++ cs336_alignment/grpo_learning_rate.sh
@@ -1,4 +1,6 @@
 
-
-uv run grpo_train_loop.py --lr 1e-6 --num_grpo_iterations 40
+# bisecitonish.
+uv run grpo_train_loop.py --lr 1e-3 --num_grpo_iterations 40
 uv run grpo_train_loop.py --lr 1e-4 --num_grpo_iterations 40
+uv run grpo_train_loop.py --lr 5e-5 --num_grpo_iterations 40
+uv run grpo_train_loop.py --lr 5e-4 --num_grpo_iterations 40
