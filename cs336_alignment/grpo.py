fb62fe8288596e0c67a2f88482bd56bcd905b52d
damek <damek@users.noreply.github.com>
2025-09-15 16:17:46 -0400

    s

--- cs336_alignment/grpo.py
+++ cs336_alignment/grpo.py
@@ -58,7 +58,7 @@ def compute_grpo_clip_loss(
     cliprange: float,
     ) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
 
-    log_ratio   = torch.clamp(policy_log_probs - old_log_probs, -20, 20) #### BE SAFE HERE
+    log_ratio   = torch.clamp(policy_log_probs - old_log_probs, -10, 10) #### BE SAFE HERE
     # log_ratio = policy_log_probs - old_log_probs
     importance_ratios = torch.exp(log_ratio.to(torch.float64)).to(torch.float32)
     # with torch.no_grad():
