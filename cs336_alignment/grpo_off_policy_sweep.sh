0eb6d983e58ac6b124ad5aa131233d0ecbc96071
damek <damek@users.noreply.github.com>
2025-09-15 17:49:32 -0400

    s

--- cs336_alignment/grpo_off_policy_sweep.sh
+++ cs336_alignment/grpo_off_policy_sweep.sh
@@ -3,5 +3,5 @@ for t in "2 256" "1 128" "4 256" "2 128" "1 64" "4 128" "2 64" "1 32"; do
     EPOCHS_PER_ROLLOUT_BATCH=$1
     TRAIN_BATCH_SIZE=$2
     GRADIENT_ACCUMULATION_STEPS=$((TRAIN_BATCH_SIZE/2))
-    uv run grpo_train_loop.py --train_batch_size $TRAIN_BATCH_SIZE --epochs_per_rollout_batch $EPOCHS_PER_ROLLOUT_BATCH --num_grpo_iterations 40 --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS --lr .75e-6 --loss_type grpo_clip --cliprange 0.2
+    uv run grpo_train_loop.py --train_batch_size $TRAIN_BATCH_SIZE --epochs_per_rollout_batch $EPOCHS_PER_ROLLOUT_BATCH --num_grpo_iterations 40 --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS --lr .75e-5 --loss_type grpo_clip --cliprange 0.2
 done
