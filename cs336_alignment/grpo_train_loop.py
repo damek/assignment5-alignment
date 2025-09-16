e556b60e6f60a6f098a1a73640f79190223617e6
damek <damek@users.noreply.github.com>
2025-09-16 15:17:16 -0400

    s

--- cs336_alignment/grpo_train_loop.py
+++ cs336_alignment/grpo_train_loop.py
@@ -201,6 +201,11 @@ for grpo_iteration in range(NUM_GRPO_ITERATIONS):
     raw_rewards = raw_rewards.to(device_hf)
     old_log_probs = torch.empty(input_ids.shape, dtype=torch.float32).to(device_hf)
 
+    # print the rollouts with new line characters
+    print("-"*100)
+    print("Here are a few rollouts: ", "\n".join([f"Prompt: {r['prompt']}\nResponse: {r['response']}\nAnswer: {r['answer']}" for r in prompt_response_answer_flattened[:3]]))
+    print("-"*100)
+
     if LOSS_TYPE == "grpo_clip" or LOSS_TYPE == "grpo_no_clip":
         print("Computing old log probs...")
         with torch.inference_mode():
